bd38ddd9fc999c716295a1c2890b87a00a9c5428
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-07-06 15:55:14 +0300

    Improve credit card class & methods

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import pandas as pd
 
-df = pd.read_csv("hotels.csv")
-
+df = pd.read_csv("hotels.csv", dtype={"id": str})
+df_card = pd.read_csv("cards.csv", dtype=str).to_dict(orient="records")
 
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
 
     def book(self):
-        pass
-
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
 
     def available(self):
         availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
@@ -23,18 +24,47 @@ class ReservationTicket:
     def __init__(self, customer_name, hotel_object):
         self.customer_name = customer_name
         self.hotel_object = hotel_object
+
     def generate(self):
-        pass
+        content = f"""
+        Thank you for your reservation.
+        Here are your booking details:
+        Customer name: {self.customer_name}
+        Hotel name: {self.hotel_object.name}
+        """
+        return content
+
+
+class CreaditCard:
+    def __init__(self,number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {"number":self.number,
+                     "expiration":expiration,
+                     "holder":holder,
+                     "cvc":cvc}
+        if card_data in df_card:
+            return True
+        else:
+            return False
 
 
 print(df)
-hotel_ID = input("Enter the id of the hotel")
+hotel_ID = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_ID)
+
 if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservartion_ticket = ReservationTicket(name, hotel)
-    print(reservartion_ticket.generate())
+    credit_card = CreaditCard(number="12345678912")
+    if credit_card.validate(expiration="12/26",
+                              holder="JOHN SMITH",
+                              cvc="123"):
+        hotel.book()
+        name = input("Enter your name: ")
+        reservartion_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+        print(reservartion_ticket.generate())
+    else:
+        print("There was a problem with your payment.")
 else:
     print("Hotel is not available...")
 
